4170f18be8dc2014bb0d1a97640aa30f75bf84e0
Rui Tammy Huang <rui.tammy.huang@gmail.com>
2026-01-02 18:24:45 -0500

    Update config.js

--- assets/js/config.js
+++ assets/js/config.js
@@ -6,4 +6,10 @@ export const SUPABASE_URL = "https://fzuslwrmhgaveulfewel.supabase.co";
 export const SUPABASE_ANON_KEY =
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZ6dXNsd3JtaGdhdmV1bGZld2VsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjczNjM0NTQsImV4cCI6MjA4MjkzOTQ1NH0.SFZJsJ3SejZsNwVtW1CcyINrrPdsRtjoWKsgNK48pbM";
 
+// This is what bootstrap.js expects to import.
+export const SUPABASE_CONFIG = {
+  url: SUPABASE_URL,
+  anonKey: SUPABASE_ANON_KEY
+};
+
 export const EDG6973_PROJECT_ID = "9dc0a871-a9fd-4898-bf8e-2d3d8692fe6d";
